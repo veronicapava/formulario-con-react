a899278e345736e1d48e731efe9b05a881050341
Veronica Pava <veronicapava@gmail.com>
2021-09-29 17:00:13 -0500

    Se estructura el formulario en la funcion App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss"
+import Field from "./components/Field"
+import Submit from "./components/Submit"
+import Form from "./components/Form"
+import Textarea from "./components/Textarea"
+import Select from "./components/Select"
+import { useRef } from "react"
 
-function App() {
+const App = () => {
+
+  const form = useRef()
+
+  const submit = e => {
+    e.preventDefault()
+    const f = form.current
+
+    const data = {
+      fullname: f.fullname.value,
+      mail: f.mail.value,
+      age: f.age.value,
+      ranking: f.ranking.value,
+      obs: f.obs.value,
+      country: f.country.value,
+      active: f.active.checked,
+    }
+
+    fetch("http://localhost:3001/info", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(resp => resp.json())
+      .then(respose => {
+        console.log(respose)
+        f.reset()
+        alert("Tus datos se han guardado")
+      })
+      .catch(e => console.error(e))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Form submit={submit} formRef={form}>
+      <Field label="Nombres" name="fullname" type="text" value="" />
+      <Field label="Correo electrónico" name="mail" type="email" value="" />
+      <Field label="Edad" name="age" type="number" value="" />
+      <Field label="Calificación" name="ranking" type="range" value="" />
+      <Textarea label="Observaciones" name="obs" value="" />
+      <Select name="country" label="País" options={
+        [{ val: "co", content: "Colombia" },
+        { val: "pe", content: "Perú" },
+        { val: "bo", content: "Bolivia" },
+        { val: "me", content: "México" }]
+      } />
+      <Field label="¿Activo?" name="active" type="checkbox" checked={true} />
+      <Submit value="Guardar" />
+    </Form>
+  )
+
 }
 
 export default App;
